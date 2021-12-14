5de39b5d84b1fe3bab832c164d3fdcac4f8622d1
pacifist24 <kosekohei2015@gmail.com>
2021-12-15 02:02:12 +0900

    shutout mobile

--- src/components/pages/Main/presenter.tsx
+++ src/components/pages/Main/presenter.tsx
@@ -9,27 +9,33 @@ import Favs from 'components/templates/Favs'
 import Search from 'components/templates/Search'
 import TLOutputText from 'components/templates/TLOutputText'
 import { ContentType } from 'ducks/main'
+import { isMobile } from 'react-device-detect'
 
 type Props = {
   displayedContent: ContentType
 }
 
 const Main: VFC<Props> = ({ displayedContent }) => (
-  <SplitPane split="vertical">
-    <Pane initialSize="240px">
-      <Sidebar />
-    </Pane>
-    <Pane initialSize="520px">
-      {displayedContent === 'tl' && <TL />}
-      {displayedContent === 'search' && <Search />}
-      {displayedContent === 'favs' && <Favs />}
-      {displayedContent === 'name' && <CharacterNameConverter />}
-      {displayedContent === 'config' && <Config />}
-    </Pane>
-    <Pane>
-      <TLOutputText />
-    </Pane>
-  </SplitPane>
+  <>
+    {isMobile && <div>モバイル端末対応は今後、TL編集機能を制限して実装予定です</div>}
+    {!isMobile && (
+      <SplitPane split="vertical">
+        <Pane initialSize="240px">
+          <Sidebar />
+        </Pane>
+        <Pane initialSize="520px">
+          {displayedContent === 'tl' && <TL />}
+          {displayedContent === 'search' && <Search />}
+          {displayedContent === 'favs' && <Favs />}
+          {displayedContent === 'name' && <CharacterNameConverter />}
+          {displayedContent === 'config' && <Config />}
+        </Pane>
+        <Pane>
+          <TLOutputText />
+        </Pane>
+      </SplitPane>
+    )}
+  </>
 )
 
 export default Main
